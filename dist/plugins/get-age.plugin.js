43a98b4a3088de8760432614453758ed81256461
BPachecoAlfaro <bpachecoalfaro@gmail.com>
2023-11-08 09:50:05 -0300

    dist agregado

---
+++ dist/plugins/get-age.plugin.js
@@ -0,0 +1,8 @@
+"use strict";
+// const getAgePlugin = require('get-age');
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAge = void 0;
+const getAge = (birthdate) => {
+    return new Date().getFullYear() - new Date(birthdate).getFullYear();
+};
+exports.getAge = getAge;
